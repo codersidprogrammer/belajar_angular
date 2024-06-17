cb75d04e139568f5ede0d0020508fac48c83feaf
codersidprogrammer <65956062+codersidprogrammer@users.noreply.github.com>
2024-06-17 21:09:16 +0700

    [update] angular lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
@@ -7,8 +17,10 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   title = 'belajar_angular';
+
+  @ViewChild('sampleDiv') elementDiv!: ElementRef<HTMLDivElement>;
 
   dummyData = {
     title: 'MyFirst Project',
@@ -16,11 +28,31 @@ export class AppComponent implements OnInit {
   };
 
   internalMessage: string = 'This is my internal message app.component';
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private route: Router
+  ) {}
 
-  constructor(private route: Router) {}
+  ngOnDestroy(): void {
+    this.dummyData = {
+      title: '',
+      content: '',
+    };
+    console.log('--- on destroy ---');
+    console.log(this.dummyData);
+  }
+  ngAfterViewInit(): void {
+    console.log('--- after view init ---');
+    console.log(this.elementDiv);
+  }
 
   ngOnInit(): void {
-    initFlowbite();
+    if (isPlatformBrowser(this.platformId)) {
+      initFlowbite();
+    }
+    console.log('--- on init ---');
+    console.log(this.elementDiv);
+    console.log(this.dummyData);
   }
 
   receiveMessage($message: string): void {
